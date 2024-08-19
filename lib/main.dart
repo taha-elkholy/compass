de60f43c04a3aa2f766d0a20062606fbf22ab94f
Islam-Shaaban-Ibrahim <eslamshaaban01120@gmail.com>
2024-08-19 09:57:04 +0300

    qiblah compass

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:compass/qiblah_compass.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  final _deviceSupport = FlutterQiblah.androidDeviceSensorSupport();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: FutureBuilder(
+          future: _deviceSupport,
+          builder: (_, AsyncSnapshot<bool?> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error: ${snapshot.error.toString()}"),
+              );
+            }
+            if (snapshot.data!) {
+              return const QiblahCompass();
+            } else {
+              return const Center(
+                child: Text("Your device is not supported"),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
